92a660dc2ca040118dfd76da2489aef5615bafec
Joby Joseph <reachjoby@gmail.com>
2023-12-21 16:57:09 -0800

    rotate image

---
+++ leetcode/0018-4sum/two-pointer.js
@@ -0,0 +1,53 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+var fourSum = function (nums, target) {
+  nums.sort((a, b) => a - b);
+  const result = [];
+
+  for (let i = 0; i < nums.length - 3; i++) {
+    // Avoid duplicates for the first element
+    if (i > 0 && nums[i] === nums[i - 1]) {
+      continue;
+    }
+
+    for (let j = i + 1; j < nums.length - 2; j++) {
+      // Avoid duplicates for the second element
+      if (j > i + 1 && nums[j] === nums[j - 1]) {
+        continue;
+      }
+
+      let left = j + 1;
+      let right = nums.length - 1;
+
+      while (left < right) {
+        const sum = nums[i] + nums[j] + nums[left] + nums[right];
+
+        if (sum === target) {
+          result.push([nums[i], nums[j], nums[left], nums[right]]);
+
+          //Avoid duplicates for the third element
+          while (left < right && nums[left] === nums[left + 1]) {
+            left++;
+          }
+
+          //Avoid duplicates for the fourth element
+          while (left < right && nums[right] === nums[right - 1]) {
+            right--;
+          }
+
+          left++;
+          right--;
+        } else if (sum < target) {
+          left++;
+        } else {
+          right--;
+        }
+      }
+    }
+  }
+
+  return result;
+};
